9c3db6fc0dff7ff99f3cdc5071525c38f60138ea
Pedro Borges <pedroborgespc@gmail.com>
2025-04-08 00:16:05 -0400

    Adding number random to star component.

--- components/rating.js
+++ components/rating.js
@@ -4,10 +4,12 @@ import StarIcon from '@mui/icons-material/Star';
 
 
 export default function HalfRating() {
+  const num = Math.floor(Math.random() * 6);
+
   return (
     <Rating 
     name="size-small" 
-    defaultValue={0}
+    defaultValue={num}
     size="small"
     precision={0.5}
     style={{ color: '#cbf203' }} 
