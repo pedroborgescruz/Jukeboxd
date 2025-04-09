324425377b3126f32544e8de4cf3dc55f8628a45
Pedro Borges <pedroborgespc@gmail.com>
2025-04-09 17:52:21 -0400

    Made library section responsive.

--- components/library.js
+++ components/library.js
@@ -5,37 +5,39 @@ import HalfRating from './rating';
 const albums = [
   { src: '/albums/lana.png', alt: 'Lana Del Rey' },
   { src: '/albums/thesecret.png', alt: 'The Secret of Us' },
-  { src: '/albums/awaken.jpeg', alt: 'Awaken, My Love!' },
   { src: '/albums/darkside.png', alt: 'Dark Side of the Moon' },
   { src: '/albums/igor.jpeg', alt: 'IGOR' },
+  { src: '/albums/lemonade.webp', alt: 'Lemonade' },
+  { src: '/albums/kate.webp', alt: 'Hounds of Love' },
 ];
 
 const ImageRow = () => {
   return (
-    <div style={{ backgroundColor: '#19121f' }} 
-      className="w-full mt-4 overflow-x-auto py-6 px-4">
-
-      <div className="mt-4 mb-15">
-        <h2 style = {{color: "#6600ff"}} className="text-center text-base/7 font-semibold">Make your voice be heard</h2>
-        <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
+    <div className="w-full mt-10 mb-10 bg-[#19121f] py-6 px-4 overflow-x-auto">
+      <div className="mb-10 text-center">
+        <h2 className="text-base font-semibold text-[#6600ff]">
+          Make your voice be heard
+        </h2>
+        <p className="mx-auto mt-2 max-w-xl text-4xl sm:text-5xl font-semibold tracking-tight text-white">
           Leave a rating or two
         </p>
       </div>
 
-      <div className="mx-auto mb-10 flex gap-6 justify-center w-fit">
+      <div className="flex gap-6 md:flex-wrap justify-center items-center w-fit md:w-full">
         {albums.map((album, index) => (
-          <div key={index} className="flex-shrink-0 flex flex-col items-center">
+          <div
+            key={index}
+            className="flex-shrink-0 flex flex-col items-center w-[160px] sm:w-[180px] md:w-[200px]"
+          >
             <Image
               src={album.src}
               alt={album.alt}
               width={200}
               height={200}
-              className="rounded-lg object-cover border-1 border-solid border-gray-700 lib-img"
+              className="rounded-lg object-cover border border-gray-700 w-full h-auto"
             />
-            <div className="mt-4"> {/* This adds spacing between the image and stars */}
+            <div className="mt-4">
               <HalfRating />
-            </div>
-            <div className="mt-2">
             </div>
           </div>
         ))}
