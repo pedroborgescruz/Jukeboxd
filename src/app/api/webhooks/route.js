3f89003f64c34deaf754eb3ca239bb6e36ab8bde
Pedro Borges <pedroborgespc@gmail.com>
2025-05-02 17:37:52 -0400

    remove comment.

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -4,7 +4,6 @@ import { clerkClient } from '@clerk/nextjs/server';
 import { createOrUpdateUser, deleteUser } from '../../lib/actions/user';
 
 export async function POST(req) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
