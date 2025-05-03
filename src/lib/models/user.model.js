079477c21d9f0ce70d1c357a88c6d1d226758e98
Pedro Borges <pedroborgespc@gmail.com>
2025-05-02 20:39:27 -0400

    fixing error with db connection.

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,43 +1,41 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-// Model what an user is under the hood for the application. 
-  
 const userSchema = new mongoose.Schema(
-    { 
-        clerkId: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-        },
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        }, 
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        avatar: {
-            type: String,
-        }, 
-        followers: {
-            type: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-            default: [],
-        }, 
-        following: {
-            type: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-            default: [],
-        }, 
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    followers: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+    following: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let initialized = false;
 
 export const connect = async () => {
-    mongoose.set('strictQuery', true);
-    if (initialized) {
-        console.log('Already connect to MongoDB.');
-        return;
-    }
+  mongoose.set('strictQuery', true);
+  if (initialized) {
+    console.log('Already connected to MongoDB');
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'Jukeboxd-App',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connect to MongoDB');
-        initialized = true;
-    } catch (error) {
-        console.log('Error connecting to MongoDB: ', error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-social-app',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+    initialized = true;
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
