af21dee30bf6fa6e3f341818552eef1240ea1731
Pedro Borges <pedroborgespc@gmail.com>
2025-05-03 00:20:27 -0400

    change schema user

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,56 +1,56 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-  {
-    clerkId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-    },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String,
-      required: true,
-    },
-    followers: {
-      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-      default: [],
-    },
-    following: {
-      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-      default: [],
-    },
-  },
-  { timestamps: true }
-);
+// const userSchema = new mongoose.Schema(
+//   {
+//     clerkId: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     email: {
+//       type: String,
+//     },
+//     firstName: {
+//       type: String,
+//       required: true,
+//     },
+//     lastName: {
+//       type: String,
+//       required: true,
+//     },
+//     username: {
+//       type: String,
+//       required: true,
+//     },
+//     avatar: {
+//       type: String,
+//       required: true,
+//     },
+//     followers: {
+//       type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+//       default: [],
+//     },
+//     following: {
+//       type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+//       default: [],
+//     },
+//   },
+//   { timestamps: true }
+// );
 
 // const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 // export default User;
 
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-// const UserSchema = new mongoose.Schema({
-//   clerkId: { type: String, required: true, unique: true },
-//   firstName: String,
-//   lastName: String,
-//   avatar: String,
-//   email: { type: String, required: true },
-//   username: String,
-// });
+const UserSchema = new mongoose.Schema({
+  clerkId: { type: String, required: true, unique: true },
+  firstName: String,
+  lastName: String,
+  avatar: String,
+  email: { type: String, required: true },
+  username: String,
+});
 
-// export default mongoose.models.User || mongoose.model('User', UserSchema);
+export default mongoose.models.User || mongoose.model('User', UserSchema);
