3ade61f60938972f7d7212d529a654ab32a6d7dd
Pedro Borges <pedroborgespc@gmail.com>
2025-05-06 22:18:47 -0400

    MongoDB schema for a review on Jukeboxd.

---
+++ src/lib/models/post.model.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose'; 
+
+// This is a MongoDB schema for a review on Jukeboxd.
+
+const reviewSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    name: { // Name of the user who wrote the review
+        type: String,
+        required: true,
+    },
+    username: { // Username of the user who wrote the review
+        type: String,
+        required: true,
+    },
+    profileImg: { // Profile image URL of the user who wrote the review
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        default: [],
+    },
+    comments: {
+        type: [
+          {
+            comment: String,
+            user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // User who wrote the comment
+            name: String, // Name of the user who wrote the comment
+            username: String, // Username of the user who wrote the comment
+            profileImg: String, // Profile image URL of the user who wrote the comment
+            createdAt: { type: Date, default: Date.now },
+          },
+        ],
+        default: [],
+    },
+  },
+  { timestamps: true } 
+);
+
+const Review = mongoose.models.Review || mongoose.model('Review', reviewSchema);
+
+export default Review;
