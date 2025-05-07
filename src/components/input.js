35c11eef731e67c63da6782ec7ff7b6d8f2e184e
Pedro Borges <pedroborgespc@gmail.com>
2025-05-06 23:10:40 -0400

    buggy input component.

--- src/components/input.js
+++ src/components/input.js
@@ -24,7 +24,6 @@ export default function Input() {
             username: user.username,
             text,
             profileImg: user.imageUrl,
-            image: imageFileUrl,
           }),
         });
         setPostLoading(false);
