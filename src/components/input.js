08a57d756247b8a9c153aea10c685cc0b1b45c7f
Pedro Borges <pedroborgespc@gmail.com>
2025-05-06 22:10:20 -0400

    add submit functionality to input for review.

--- src/components/input.js
+++ src/components/input.js
@@ -10,6 +10,26 @@ export default function Input() {
         return null;
     }
 
+    const handleSubmit = async () => {
+        setPostLoading(true);
+        const response = await fetch('/api/post/create', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userMongoId: user.publicMetadata.userMongoId,
+            name: user.fullName,
+            username: user.username,
+            text,
+            profileImg: user.imageUrl,
+            image: imageFileUrl,
+          }),
+        });
+        setText('');
+        location.reload();
+    };
+
     return (
         <div className='flex border-b border-gray-700 p-3 mb-5 space-x-3 w-130'>
             <img
@@ -27,7 +47,8 @@ export default function Input() {
             <div className='flex items-center justify-between ml-3 pt-2.5'>
                 <button
                 disabled={text.trim() === '' || postLoading}
-                className='bg-primary text-white px-4 py-1.5 rounded-full shadow-md hover:brightness-95 disabled:opacity-50'>
+                className='bg-primary text-white px-4 py-1.5 rounded-full shadow-md hover:brightness-95 disabled:opacity-50'
+                onClick={handleSubmit}>
                     Review
                 </button>
             </div>           
