03b0a820d1d06ce98c5579adbbc2c24e72443de1
Pedro Borges <pedroborgespc@gmail.com>
2025-05-12 00:47:20 -0400

    added review area on album page.

--- src/components/input.js
+++ src/components/input.js
@@ -27,6 +27,7 @@ export default function Input() {
         });
         setPostLoading(false);
         setText('');
+        location.reload();
     };
 
     if (!isSignedIn || !isLoaded) {
--- src/components/review.js
+++ src/components/review.js
@@ -1,7 +1,39 @@
-export default function Review() {
+import Link from 'next/link';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import moment from 'moment';
+
+export default function Review({review}) {
     return (
-        <div>
-            <h1>test1</h1>
+     <div className='flex p-3 border-b border-gray-800 w-130'>
+      <Link href={`/users/${review?.username}`}>
+        <img
+          src={review?.profileImg}
+          alt='user-img'
+          className='h-11 w-11 rounded-full mr-4'
+        />
+      </Link>
+      <div className='flex-1'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center space-x-1 whitespace-nowrap'>
+            <h4 className='font-bold text-xs truncate max-w-32'>
+              {review?.name}
+            </h4>
+            <span className='text-xs truncate max-w-32'>@{review?.username}</span>
+            {/* add dot space here */}
+            <span className='text-xl text-gray-500'>·</span>
+            <span className='text-xs text-gray-500 flex-1 truncate max-w-32'>
+              {moment(review?.createdAt).fromNow()}
+            </span>
+          </div>
+          <HiDotsHorizontal className='text-sm' />
         </div>
+        <Link href={`/reviews/${review?._id}`}>
+          <p className='text-gray-300 text-sm my-3 w-full'>{review?.text}</p>
+        </Link>
+        <Link href={`/reviews/${review?._id}`}>
+          <img src={review?.image} className='rounded-2xl mr-2' />
+        </Link>
+      </div>
+    </div>
     )
 }
