297b3c407c3ebf2419bf35c640235ff020091c13
Pedro Borges <pedroborgespc@gmail.com>
2025-05-19 23:53:11 -0400

    Added comments to album page.

--- src/app/(others)/albums/[album]/page.js
+++ src/app/(others)/albums/[album]/page.js
@@ -7,6 +7,9 @@ export default async function App() {
 
   let data = null;
 
+  // Fetch all reviews.
+  {/* Note: Will need to write API route handler to fetch reviews for a given 
+  // album, not ALL reviews in DB. */}
   try {
     const result = await fetch(process.env.NEXT_PUBLIC_URL + '/api/review/all', {
       method: 'POST',
@@ -20,15 +23,15 @@ export default async function App() {
 
   return (
     <div className="grid grid-cols-12 gap-4 mb-20">
-      {/* First Column - 3/12 (1/4) */}
+      {/* First Column */}
       <div className="col-span-3">
         <Album />
       </div>
-      {/* Second Column - 3/12 (1/4) */}
+      {/* Second Column */}
       <div className="col-span-3">
         <AlbumActions />
       </div>
-      {/* Third Column - 6/12 (1/2) */}
+      {/* Third Column */}
       <div className="col-span-6 space-y-4 mt-20">
         <div className="flex flex-col items-center">
           <small className="text-gray-500">POPULAR REVIEWS</small>
